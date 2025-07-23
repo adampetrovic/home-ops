55560c8525d396ab960f2bddb2666019ebba16d5
Adam Petrovic <apetrovic@atlassian.com>
2025-07-24 06:44:09 +1000

    more changes

--- kubernetes/apps/network/cloudflare-ddns/app/cloudflare-ddns.sh
+++ kubernetes/apps/network/cloudflare-ddns/app/cloudflare-ddns.sh
@@ -7,14 +7,14 @@ current_ipv4="$(curl -s https://ipv4.icanhazip.com/)"
 zone_id=$(curl -s -X GET \
     "https://api.cloudflare.com/client/v4/zones?name=${CLOUDFLARE_RECORD_NAME#*.}&status=active" \
     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
-    -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
+    -H "X-Auth-Key: ${CLOUDFLARE_API_TOKEN}" \
     -H "Content-Type: application/json" \
         | jq --raw-output ".result[0] | .id"
 )
 record_ipv4=$(curl -s -X GET \
     "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${CLOUDFLARE_RECORD_NAME}&type=A" \
     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
-    -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
+    -H "X-Auth-Key: ${CLOUDFLARE_API_TOKEN}" \
     -H "Content-Type: application/json" \
 )
 old_ip4=$(echo "$record_ipv4" | jq --raw-output '.result[0] | .content')
@@ -26,7 +26,7 @@ record_ipv4_identifier="$(echo "$record_ipv4" | jq --raw-output '.result[0] | .i
 update_ipv4=$(curl -s -X PUT \
     "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_ipv4_identifier}" \
     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
-    -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
+    -H "X-Auth-Key: ${CLOUDFLARE_API_TOKEN}" \
     -H "Content-Type: application/json" \
     --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":false,\"name\":\"${CLOUDFLARE_RECORD_NAME}\",\"content\":\"${current_ipv4}\"}" \
 )
